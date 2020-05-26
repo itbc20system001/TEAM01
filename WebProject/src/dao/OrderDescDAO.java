4ffdeaf07b4cc8ce25f7f3a6d7f9433f9109d185
anzai <anzai@NA19690.nature.local>
2020-05-26 16:30:43 +0900

    「OrderDescDAO」の作成

---
+++ WebProject/src/dao/OrderDescDAO.java
@@ -0,0 +1,54 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javabeans.HappyLife;
+
+
+public class OrderDescDAO {
+	//データベース接続に使用する情報
+	private final String JDBC_URL = "";//未定
+	private final String DB_USER = "root";
+	private final String DB_PASS = "insource.2015it";
+
+	public OrderDescDAO() {
+		// TODO 自動生成されたコンストラクター・スタブ
+		try {
+			Class.forName("");//未定
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	public boolean create(HappyLife happylife) {
+		//データベース接続
+		try (Connection conn = DriverManager.getConnection(
+				JDBC_URL, DB_USER, DB_PASS)) {
+
+			//INSERT文の準備
+			String sql = "INSERT INTO ORDER_DESC(PO_ID, P_id) VALUES(?,?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			//INSERT文中の「？」に使用する値を設定しSQLを完成
+			pStmt.setInt(1, happylife.getProductId());//po_idの取得と設定。
+			//pStmt.setInt(2, happylife.get));//p_idの取得と設定。未確定
+
+			//INSERT文の実行
+			int result = pStmt.executeUpdate();
+			if (result != 1) {
+				return false;
+			}
+
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+
+}
