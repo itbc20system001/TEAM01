a9e0bcfdccaf9bcaa05931752ca6800e873196d0
chonan <chonan@NA19606.nature.local>
2020-05-27 13:51:46 +0900

    HappyLife.javaからemailとpasswordの変数を削除。usrDAOにhappypointの処理を追加。

--- WebProject/src/dao/UsrDAO.java
+++ WebProject/src/dao/UsrDAO.java
@@ -5,24 +5,30 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import javabeans.HappyLife;
 import javabeans.Register;
 
 public class UsrDAO {
 	//データベース接続に使用する情報
-	private final String JDBC_URL = "jdbc:mariadb://localhost/kohukudo";//未定
+	private final String JDBC_URL = "jdbc:mariadb://localhost/kohukudo";
 	private final String DB_USER = "root";
 	private final String DB_PASS = "insource.2015it";
 
 
-
 	public boolean create(Register register) {
+
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
 		//データベース接続
 		try (Connection conn = DriverManager.getConnection(
 				JDBC_URL, DB_USER, DB_PASS)) {
 
 			//INSERT文の準備
 			String sql =
-			  "INSERT INTO USR(L_NAME,F_NAME,L_NAME_KANA,F_NAME_KANA,PREFECTURE,CITY,O_ADDRESS,TEL,EMAIL,BIRTHDAY,PASSWORD) VALUES(?,?,?,?,?,?,?,?,?,?)";
+					"INSERT INTO USR(L_NAME,F_NAME,L_NAME_KANA,F_NAME_KANA,PREFECTURE,CITY,O_ADDRESS,TEL,EMAIL,BIRTHDAY,PASSWORD) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			//INSERT文中の「？」に使用する値を設定しSQLを完成
@@ -38,6 +44,39 @@ public class UsrDAO {
 			pStmt.setString(10, register.getBirthday());
 			pStmt.setString(11, register.getPassword());
 
+			//INSERT文の実行
+			int result = pStmt.executeUpdate();
+			if (result != 1) {
+				return false;
+			}
+
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+
+	public boolean create(HappyLife happylife) {
+
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		//データベース接続
+		try (Connection conn = DriverManager.getConnection(
+				JDBC_URL, DB_USER, DB_PASS)) {
+
+			//INSERT文の準備
+			String sql =
+					"INSERT INTO USR(HAPPYPOINT) VALUES(?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			//INSERT文中の「？」に使用する値を設定しSQLを完成
+			pStmt.setInt(1, happylife.getHappypoint());
 
 
 			//INSERT文の実行
@@ -55,3 +94,5 @@ public class UsrDAO {
 	}
 
 }
+
+
