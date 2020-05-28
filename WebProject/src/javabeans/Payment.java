e8dfd242f5dac9568815dcce53f4c6d0756aed9b
anzai <anzai@NA19690.nature.local>
2020-05-28 11:50:31 +0900

    HappyCalcLogicとpaymentの作成

---
+++ WebProject/src/javabeans/Payment.java
@@ -0,0 +1,22 @@
+package javabeans;
+
+public class Payment {
+
+	private int sum;//購入金額
+	private int change;//おつり
+
+	public int getSum() {
+		return sum;
+	}
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+	public int getChange() {
+		return change;
+	}
+	public void setChange(int change) {
+		this.change = change;
+	}
+
+
+}
