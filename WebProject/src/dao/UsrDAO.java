6ef35137ce0add0a4a5877c22386246ecd56c21f
anzai <anzai@NA19690.nature.local>
2020-05-29 15:27:29 +0900

    会員情報の表示

--- WebProject/src/dao/UsrDAO.java
+++ WebProject/src/dao/UsrDAO.java
@@ -15,7 +15,7 @@ public class UsrDAO {
 	private final String DB_USER = "root";
 	private final String DB_PASS = "insource.2015it";
 
-
+	//現在未使用
 	public boolean create(Register register) {
 
 		try {
@@ -28,8 +28,7 @@ public class UsrDAO {
 				JDBC_URL, DB_USER, DB_PASS)) {
 
 			//INSERT文の準備
-			String sql =
-					"INSERT INTO USR(L_NAME,F_NAME,L_NAME_KANA,F_NAME_KANA,PREFECTURE,CITY,O_ADDRESS,TEL,EMAIL,BIRTHDAY,PASSWORD) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+			String sql = "INSERT INTO USR(L_NAME,F_NAME,L_NAME_KANA,F_NAME_KANA,PREFECTURE,CITY,O_ADDRESS,TEL,EMAIL,BIRTHDAY,PASSWORD) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			//INSERT文中の「？」に使用する値を設定しSQLを完成
@@ -72,8 +71,7 @@ public class UsrDAO {
 				JDBC_URL, DB_USER, DB_PASS)) {
 
 			//INSERT文の準備
-			String sql =
-					"INSERT INTO USR(L_NAME,F_NAME,L_NAME_KANA,F_NAME_KANA,PREFECTURE,CITY,O_ADDRESS,TEL,EMAIL,BIRTHDAY,PASSWORD,HAPPY) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
+			String sql = "INSERT INTO USR(L_NAME,F_NAME,L_NAME_KANA,F_NAME_KANA,PREFECTURE,CITY,O_ADDRESS,TEL,EMAIL,BIRTHDAY,PASSWORD,HAPPY) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			//INSERT文中の「？」に使用する値を設定しSQLを完成
@@ -115,15 +113,14 @@ public class UsrDAO {
 		try (Connection conn = DriverManager.getConnection(
 				JDBC_URL, DB_USER, DB_PASS)) {
 
-
 			String sql = "select email, password from usr where email = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 			pStmt.setString(1, register.getEmail());
 
 			ResultSet rs = pStmt.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				String email = rs.getString("email");
-				if(email.equals(register.getEmail())) {
+				if (email.equals(register.getEmail())) {
 					return true;
 				}
 			}
@@ -135,7 +132,6 @@ public class UsrDAO {
 		}
 		return false;
 	}
-
 
 	public boolean create2(HappyLife happylife) {
 
@@ -149,13 +145,12 @@ public class UsrDAO {
 				JDBC_URL, DB_USER, DB_PASS)) {
 
 			//INSERT文の準備
-			String sql =
-					"UPDATE usr SET HAPPY = ? WHERE USER_ID = ?";
+			String sql = "UPDATE usr SET HAPPY = ? WHERE USER_ID = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			//INSERT文中の「？」に使用する値を設定しSQLを完成
 			pStmt.setInt(1, happylife.getHappypoint());
-			pStmt.setInt(2,happylife.getUserid());
+			pStmt.setInt(2, happylife.getUserid());
 
 			//INSERT文の実行
 			int result = pStmt.executeUpdate();
@@ -171,6 +166,55 @@ public class UsrDAO {
 		return true;
 	}
 
+	public Register findUserInfo(Register reg ) {
+		//既に登録されてある会員情報をデータベースから取得しRegister.javaにセットする処理
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+
+		//データベース接続
+		try (Connection conn = DriverManager.getConnection(
+				JDBC_URL, DB_USER, DB_PASS)) {
+
+			String sql = "select * from usr where ";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			ResultSet rs = pStmt.executeQuery();
+
+			if (rs.next()) {
+				//Register.javaに会員情報をセット
+
+
+				String l_name = rs.getString("l_name");
+				reg.setL_name(l_name);
+				String f_name = rs.getString("f_name");
+				reg.setF_name(f_name);
+				String l_name_kana = rs.getString("l_name_kana");
+				reg.setL_name_kana(l_name_kana);
+				String f_name_kana = rs.getString("f_name_kana");
+				reg.setF_name_kana(f_name_kana);
+				String prefecture = rs.getString("prefecture");
+				reg.setPrefecture(prefecture);
+				String city = rs.getString("city");
+				reg.setCity(city);
+				String o_address = rs.getString("o_address");
+				reg.setO_address(o_address);
+				String tel = rs.getString("tel");
+				reg.setTel(tel);
+				String email = rs.getString("email");
+				reg.setEmail(email);
+				String birthday = rs.getString("birthday");
+				reg.setBirthday(birthday);
+				String password = rs.getString("password");
+				reg.setPassword(password);
+			}
+
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return null;
+		}
+		return reg;
+	}
 }
-
-
